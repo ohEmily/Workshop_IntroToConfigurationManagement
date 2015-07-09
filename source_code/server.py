b40921e2e03ec9fc2a161653cfc83ff528e64208
Emily Pakulski <emily.pakulski@mongodb.com>
2015-07-09 19:07:51 -0400

    Fixed advanced playbook

--- source_code/server.py
+++ source_code/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-        return "Hello, 2015 hackNY fellows!"
+    return "Hello, 2015 hackNY fellows!"
 
-    if __name__ == "__main__":
-            app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
